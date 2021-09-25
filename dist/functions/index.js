7ff927fc1eae5d13cff1b0ad010abd27d2c41d21
V. Sai Prakash <vukkumsp@gmail.com>
2021-09-25 18:55:07 +0000

    adding the dist folder also into git cause it contains some changes for firebase cloudfn deployment;

---
+++ dist/functions/index.js
@@ -0,0 +1,14 @@
+const functions = require("firebase-functions");
+const mainjsFile = require(__dirname + "/server/main");
+
+exports.handler = functions
+// Changing the location of function to Indian region
+                    .region('asia-south1')
+                    .https.onRequest(mainjsFile.app());
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
